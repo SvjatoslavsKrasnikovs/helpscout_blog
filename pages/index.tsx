73abb61f001feb4cf80cec50f370a8ba1ca4985a
SvjatoslavsKrasnikovs <svjatoslavs.krasnikovs@gmail.com>
2020-10-09 08:24:25 +0300

    Cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,11 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import React from 'react'
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+const Index: React.FC = () => {
+  return (
+    <div>
+      <p>Hello!!</p>
+    </div>
+  )
+}
 
-export default IndexPage
+export default Index
